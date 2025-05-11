f5e7444b7b79dd6345fab09ab99b554d8154005d
Mars <marosakraid@gmail.com>
2025-05-11 17:55:14 +0500

    Добавлены новые тесты

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -4,34 +4,58 @@ import org.junit.jupiter.api.Assertions;
 public class BonusServiceTest {
 
     @Test
+        // Незарегистрированный пользователь, бонус меньше лимита
     void shouldCalculateForNotRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
-        // подготавливаем данные:
         long amount = 1000;
         boolean registered = false;
         long expected = 10;
 
-        // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
+        // Незарегистрированный пользователь, бонус больше лимита
     void shouldCalculateForNotRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
-        // подготавливаем данные:
         long amount = 1_000_000;
         boolean registered = false;
         long expected = 500;
 
-        // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
 
-        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+        // Зарегистрированный пользователь, бонус меньше лимита
+    void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, registered);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+        // зарегистрированный пользователь, бонус больше лимита
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
         Assertions.assertEquals(expected, actual);
     }
 }
